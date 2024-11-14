a96c0c74e04435444b8377a7b50d65b5e3f71381
Nick_Belyak <42941752+Nikita-Belyakov@users.noreply.github.com>
2024-11-14 18:10:02 +0300

    Add files via upload

---
+++ FDA_DH_bicubic_match_augmentation.py
@@ -0,0 +1,27 @@
+import numpy as np
+import albumentations as A
+import torch
+class FDA_mask_Transform(torch.nn.Module):
+    def __init__(self,p=1.0):
+        super().__init__()
+        self.p = p
+
+    def forward(self, x_tif, mask):
+        eps = np.random.random()
+        if self.p>eps:
+            mask_ = (torch.tensor(mask).clone()).numpy()
+            mask_[mask_>=1] = 1 # leave only 1 class in mask for further masking clouds and shadows regions for FDA
+            # print(np.unique(mask))
+            inv_mask = (torch.tensor(mask_).clone()).numpy()
+            inv_mask = np.abs(inv_mask-1)
+            inv_mask[inv_mask!=0] = 1
+            # print(np.unique(inv_mask))
+            corrupted_bands = (torch.tensor(x_tif).clone()).numpy()
+            # corrupted_bands = corrupted_bands*mask
+            l8_bicubic, l8_DH = corrupted_bands[:6], corrupted_bands[6:]
+            aug_FDA = A.Compose([A.FDA([l8_bicubic.transpose(1,2,0)], p=1, read_fn=lambda x: x)])
+            fda_res = aug_FDA(image=l8_DH.transpose(1,2,0))['image']
+            fda_res = fda_res.transpose(2,0,1)
+            x_tif_masked = inv_mask*x_tif[6:]
+            x_tif = x_tif_masked + fda_res*mask_
+        return x_tif
